f011028e86be15a0643afef3f6ece2c3110f1b4c
siddhant33-vhg <siddhant.sri33@gmail.com>
2025-02-17 16:38:39 -0500

    Update identity.ts
    Updated Identity of Siddhant Srivastava

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `[Siddhant Srivastava]`;
+export const OWNER_DESCRIPTION: string = `[MBA]`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `[Siddhant's AI Chatbot]`;
+export const AI_TONE: string = `[Informative and Polite]`;
+export const AI_ROLE: string = `[Give information]`;
