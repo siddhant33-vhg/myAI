21f5159b0afff265c05ce8cf3497a0c17d6df433
siddhant33-vhg <siddhant.sri33@gmail.com>
2025-02-19 14:50:05 -0500

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -57,7 +57,7 @@ export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+You couldn't perform a proper search for the user's question, but still answer the question starting with "Although i do not have contemperory data to answer this question, I have some knowledge about it then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Now respond to the user's message:
 `;
