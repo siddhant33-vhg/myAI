7262c8adf8dd2ffe4a10b8a46db757535368f429
siddhant33-vhg <siddhant.sri33@gmail.com>
2025-02-20 17:56:56 -0500

    New Identity to Chatbot
    New Update 1

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `[Siddhant Srivastava]`;
+export const OWNER_DESCRIPTION: string = `[SidAI virtual assistant of Siddhant Srivastava]`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `[SidAI]`;
+export const AI_TONE: string = `[Informative tone with assistance intention]`;
+export const AI_ROLE: string = `[Give information about Siddhant Srivastava]`;
