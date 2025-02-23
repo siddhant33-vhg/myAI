b3724d1a4c0b2bcade3a4c8eaf70c85cb22fabf5
siddhant33-vhg <siddhant.sri33@gmail.com>
2025-02-23 01:32:35 -0500

    Update footer.tsx

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -1,30 +1,9 @@
-import { FOOTER_MESSAGE } from "@/configuration/ui";
-import Link from "next/link";
-
 export default function ChatFooter() {
   return (
-    <div className="w-full text-xs flex pt-2 text-gray-500">
-      <div className="flex-grow text-left">
-        {/* Left Pane */}
-        <Link href="/terms" className="hover:underline">
-          Terms of Service
-        </Link>
-      </div>
-      <div className="flex-grow text-center">
-        {/* Center Pane */}
-        {FOOTER_MESSAGE}
-      </div>
-      <div className="flex-grow text-right">
-        {/* Right Pane */}
-        {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
-        <a
-          href="http://www.ringel.ai"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          powered by ringel.AI
-        </a>
-      </div>
+    <div className="footer">
+      <div className="terms">Terms of Service</div>
+      <div className="customized">Customized AI by Siddhant Srivastava</div>
+      <div className="powered">powered by ringel.AI</div>
     </div>
   );
 }
