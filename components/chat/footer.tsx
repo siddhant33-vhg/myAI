9d800f0386f7979145faaa85d7e4a117de0f5a03
siddhant33-vhg <siddhant.sri33@gmail.com>
2025-03-01 18:25:24 -0500

    Update footer.tsx

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -3,7 +3,7 @@ export default function ChatFooter() {
     <div className="footer">
       <div className="terms">Terms of Service</div>
       <div className="customized">Customized AI by Siddhant Srivastava</div>
-      <div className="powered">powered by ringel.AI</div>
+      <div className="powered">powered by Synergy 33°</div>
     </div>
   );
 }
